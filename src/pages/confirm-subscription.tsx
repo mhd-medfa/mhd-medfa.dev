d2b1094568b32e55fffa7d6a4c28f68ccd3a0f65
Mohamad Al Mdfaa <mohamad.almdfaa@gmail.com>
2023-08-05 21:20:53 +0000

    fix: fix the layout and style of the confirm subscription page

--- src/pages/confirm-subscription.tsx
+++ src/pages/confirm-subscription.tsx
@@ -1,11 +1,16 @@
-import { HeadFC, Link, PageProps } from "gatsby";
+import { graphql, HeadFC, Link, PageProps } from "gatsby";
 import * as React from "react";
 
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-};
+import Layout from "../components/layout";
+
+interface IPageData {
+  homepage: Queries.PrismicHomepage;
+}
+// const pageStyles = {
+//   color: "#232129",
+//   padding: "96px",
+//   fontFamily: "-apple-system, Roboto, sans-serif, serif",
+// };
 const headingStyles = {
   marginTop: 0,
   marginBottom: 64,
@@ -23,29 +28,57 @@ const codeStyles = {
   borderRadius: 4,
 };
 
-const SubscriptionConfirmedPage: React.FC<PageProps> = () => {
+const SubscriptionConfirmedPage: React.FC<PageProps<IPageData>> = ({ data }) => {
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>Subscription confirmed!</h1>
-      <p style={paragraphStyles}>
-        Boom!😀 You're officially confirmed and on the list. Expect some great emails headed your way very soon.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <b>
-          <Link to="/">Go home</Link>.
-        </b>
-      </p>
-    </main>
+    <Layout githubUrl={data.homepage.data.github!.url!} cvUrl={data.homepage.data.cv!.url!}>
+      <main>
+        {" "}
+        {/* <main>  style={pageStyles}> */}
+        <h1 style={headingStyles}>Subscription confirmed!</h1>
+        <p style={paragraphStyles}>
+          Boom!😀 You're officially confirmed and on the list. Expect some great emails headed your way very soon.
+          <br />
+          {process.env.NODE_ENV === "development" ? (
+            <>
+              <br />
+              Try creating a page in <code style={codeStyles}>src/pages/</code>.
+              <br />
+            </>
+          ) : null}
+          <br />
+          <b>
+            <Link to="/">Go home</Link>.
+          </b>
+        </p>
+      </main>
+    </Layout>
   );
 };
 
 export default SubscriptionConfirmedPage;
 
 export const Head: HeadFC = () => <title>ML Engineer Newsletter - Subscription Confirmed</title>;
+
+export const query = graphql`
+  query ($language: String!) {
+    locales: allLocale(filter: { language: { eq: $language } }) {
+      edges {
+        node {
+          ns
+          data
+          language
+        }
+      }
+    }
+    homepage: prismicHomepage {
+      data {
+        cv {
+          url
+        }
+        github {
+          url
+        }
+      }
+    }
+  }
+`;
