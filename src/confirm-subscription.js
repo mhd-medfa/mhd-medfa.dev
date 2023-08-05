329826a20209ba35f190ade677d159ced865196e
Mohamad Al Mdfaa <mohamad.almdfaa@gmail.com>
2023-08-05 19:55:53 +0000

    feat: add /confirm-subscription

---
+++ src/confirm-subscription.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class ConfirmSubscription extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Subscription confirmed!</h1>
+        <p>Boom! You're officially confirmed and on the list. Expect some great emails headed your way very soon.</p>
+      </div>
+    );
+  }
+}
+
+export default ConfirmSubscription;
